d06bae95a4a02bc4a2caf21335c6144f8d02602b
osx user <gili.shemesh@gmail.com>
2022-09-02 01:05:58 +0300

    commit

--- libs/TestRtAudio/TestRtAudioLib/Components/Envelopes/LinearAdsr.cpp
+++ libs/TestRtAudio/TestRtAudioLib/Components/Envelopes/LinearAdsr.cpp
@@ -1,7 +1,64 @@
+#include "Components/GateConstants.h"
 #include "LinearAdsr.h"
+
 namespace RtAudioNs
 {
   namespace Components
-  {
+    {
 
-  }}
+
+    void LinearAdsrComponent::render(std::vector<double> &vGate, std::vector<double> &vOut)
+    {
+
+
+
+      for (unsigned int i = 0; i < vGate.size(); i++)
+      {
+        if (vGate[i] > gateThreshold)
+        {
+          if (simpleAdsrStatus == SimpleAdsrStatus::idle || simpleAdsrStatus == SimpleAdsrStatus::r)
+          {
+            vOut[i] = stepA.proccess(true);
+          }
+          else if (simpleAdsrStatus == SimpleAdsrStatus::a)
+          {
+            vOut[i] = stepA.proccess(false);
+          }
+          else if (simpleAdsrStatus == SimpleAdsrStatus::beforeD)
+          {
+            vOut[i] = stepD.proccess(true);
+          }
+          else if (simpleAdsrStatus == SimpleAdsrStatus::d)
+          {
+            vOut[i] = stepD.proccess(false);
+          }
+          else if (simpleAdsrStatus == SimpleAdsrStatus::s)
+          {
+            vOut[i] = sustainLevel;
+          }
+        }
+        else
+        {
+          if (simpleAdsrStatus == SimpleAdsrStatus::a ||
+              simpleAdsrStatus == SimpleAdsrStatus::beforeD ||
+              simpleAdsrStatus == SimpleAdsrStatus::d ||
+              simpleAdsrStatus == SimpleAdsrStatus::s)
+          {
+            vOut[i] = stepR.proccess(true);
+          }
+          else if (simpleAdsrStatus == SimpleAdsrStatus::r)
+          {
+            vOut[i] = stepR.proccess(false);
+          }
+          else
+          {
+            simpleAdsrStatus = SimpleAdsrStatus::idle;
+            vOut[i] = 0;
+          }
+        }
+      }
+    }
+
+  }
+  
+  }
--- libs/TestRtAudio/TestRtAudioLib/Components/Envelopes/LinearAdsr.h
+++ libs/TestRtAudio/TestRtAudioLib/Components/Envelopes/LinearAdsr.h
@@ -1,8 +1,123 @@
 #pragma once
+#include <vector>
+#include <cmath>
+#include "AdsrStep.h"
 
 namespace RtAudioNs
 {
   namespace Components
   {
+    class LAdsrStepA : public AdsrStep
+    {
+    public:
+      LAdsrStepA(
+          double &_returnVal,
+          SimpleAdsrStatus &_simpleAdsrStatus,
+          SimpleAdsrStatus _currentStatus,
+          SimpleAdsrStatus _beforeNextStatus) : AdsrStep(_returnVal, _simpleAdsrStatus, _currentStatus, _beforeNextStatus) {}
 
-  }}
+      void updateReturnVal()
+      {
+        returnVal = returnVal + incrementStep;
+      }
+      void resetRetval()
+      {
+        returnVal = 0;
+      }
+      bool moveNextStateCondition()
+      {
+        return returnVal == 1;
+      }
+      double incrementStep = 0.02;
+    };
+
+    class LAdsrStepD : public AdsrStep
+    {
+    public:
+      LAdsrStepD(
+          double &_sustainLevel,
+          double &_returnVal,
+          SimpleAdsrStatus &_simpleAdsrStatus,
+          SimpleAdsrStatus _currentStatus,
+          SimpleAdsrStatus _beforeNextStatus) : AdsrStep(_returnVal, _simpleAdsrStatus, _currentStatus, _beforeNextStatus),
+                                                sustainLevel{_sustainLevel} {}
+
+      void updateReturnVal()
+      {
+        returnVal = returnVal + incrementStep;
+      }
+      void resetRetval()
+      {
+        returnVal = 1;
+      }
+      bool moveNextStateCondition()
+      {
+        return returnVal == sustainLevel;
+      }
+      
+      double incrementStep = -0.02;
+    private:
+      double &sustainLevel;
+    };
+
+    class LAdsrStepR : public AdsrStep
+    {
+    public:
+      LAdsrStepR(
+          double &_returnVal,
+          SimpleAdsrStatus &_simpleAdsrStatus,
+          SimpleAdsrStatus _currentStatus,
+          SimpleAdsrStatus _beforeNextStatus) : AdsrStep(_returnVal, _simpleAdsrStatus, _currentStatus, _beforeNextStatus) {}
+
+      void updateReturnVal()
+      {
+        if (returnVal > 0)
+        {
+          returnVal = returnVal + incrementStep;
+        }
+        else
+        {
+          returnVal = 0;
+        }
+
+      }
+      void resetRetval()
+      {
+        sustainLevel = returnVal;
+      }
+      bool moveNextStateCondition()
+      {
+        return returnVal == 0;
+      }
+
+      double incrementStep = -0.02;
+      
+
+    private:
+      double sustainLevel = 0;
+    };
+
+    
+    class LinearAdsrComponent
+    {
+    public:
+      void render(std::vector<double> &vGate, std::vector<double> &vOut);
+      LinearAdsrComponent() : stepA(returnVal, simpleAdsrStatus, SimpleAdsrStatus::a, SimpleAdsrStatus::beforeD),
+                              stepD(sustainLevel, returnVal, simpleAdsrStatus, SimpleAdsrStatus::d, SimpleAdsrStatus::s),
+                              stepR(returnVal, simpleAdsrStatus, SimpleAdsrStatus::r, SimpleAdsrStatus::idle)
+      {
+      }
+
+      double sustainLevel = 0.7;
+
+    private:
+      double returnVal = 0;
+
+      SimpleAdsrStatus simpleAdsrStatus = SimpleAdsrStatus::idle;
+
+      LAdsrStepA stepA;
+      LAdsrStepD stepD;
+      LAdsrStepR stepR;
+    };
+  }
+}
